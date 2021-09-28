d533cee24a6c5a94e062b44a98373ef15c4dc4e3
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-28 22:33:06 +0300

    delete useless photos

--- jobs/settings.py
+++ jobs/settings.py
@@ -91,9 +91,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
+ 
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
         "OPTIONS": {
--- jobsapp/templatetags/is_already_applied.py
+++ jobsapp/templatetags/is_already_applied.py
@@ -3,8 +3,6 @@ from django import template
 from jobsapp.models import Applicant
 
 register = template.Library()
-
-
 @register.simple_tag(name="is_already_applied")
 def is_already_applied(job, user):
     applied = Applicant.objects.filter(job=job, user=user)
