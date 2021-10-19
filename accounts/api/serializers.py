655847b27c3001d0025be07f0bc86c3cf4adbe5e
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-19 23:24:50 +0300

    remove comment

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -10,7 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        # fields = "__all__"
         exclude = (
             "password",
             "user_permissions",
